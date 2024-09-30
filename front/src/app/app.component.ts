83d3344016f075b9785226e6cc48c37bce058a4a
GuillaumeSUBRA1 <74560838+GuillaumeSUBRA1@users.noreply.github.com>
2024-09-30 22:05:41 +0200

    init front

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FaIconLibrary, FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { fontAwesomeIcons } from './shared/font-awesome-icons';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FontAwesomeModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  faIconLibrary = inject(FaIconLibrary);
+
+  ngOnInit(): void {
+    this.initIcons();
+  }
+
+  initIcons(){
+    this.faIconLibrary.addIcons(...fontAwesomeIcons);
+  }
+
 }
