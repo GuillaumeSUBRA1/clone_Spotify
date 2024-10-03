5ec9df1781b52ca4183df561ad7867c269cd704c
GuillaumeSUBRA1 <74560838+GuillaumeSUBRA1@users.noreply.github.com>
2024-10-03 09:25:04 +0200

    change package name + clean code + add auth0 and mapstruct

--- back/src/main/java/com/example/back/BackApplication.java
+++ back/src/main/java/com/example/back/BackApplication.java
@@ -1,4 +1,4 @@
-package fr.guillaumesubra1.back;
+package com.example.back;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
