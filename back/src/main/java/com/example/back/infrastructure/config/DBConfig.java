d1b477bc225be129a88bd1ffa07e412af8f6a38e
GuillaumeSUBRA1 <74560838+GuillaumeSUBRA1@users.noreply.github.com>
2024-10-03 09:27:19 +0200

    add config

---
+++ back/src/main/java/com/example/back/infrastructure/config/DBConfig.java
@@ -0,0 +1,13 @@
+package com.example.back.infrastructure.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableJpaRepositories({"com.example.back.repository"})
+@EnableTransactionManagement
+@EnableJpaAuditing
+public class DBConfig {
+}
