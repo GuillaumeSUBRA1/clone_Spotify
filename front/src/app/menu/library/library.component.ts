bbee514094021a0caf834fa694a94224819d382d
GuillaumeSUBRA1 <74560838+GuillaumeSUBRA1@users.noreply.github.com>
2024-10-04 09:36:50 +0200

    add library

--- front/src/app/menu/library/library.component.ts
+++ front/src/app/menu/library/library.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { FaIconComponent } from '@fortawesome/angular-fontawesome';
 
 @Component({
   selector: 'library',
   standalone: true,
   imports: [
-    FaIconComponent
+    FaIconComponent,
+    RouterModule
   ],
   templateUrl: './library.component.html',
   styleUrl: './library.component.scss'
