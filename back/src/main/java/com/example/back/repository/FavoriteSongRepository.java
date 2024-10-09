b405b76965cbb383d64e72dbf09adb83b45ef758
GuillaumeSUBRA1 <74560838+GuillaumeSUBRA1@users.noreply.github.com>
2024-10-09 22:21:41 +0200

    add favorite button

--- back/src/main/java/com/example/back/repository/FavoriteSongRepository.java
+++ back/src/main/java/com/example/back/repository/FavoriteSongRepository.java
@@ -1,9 +1,14 @@
 package com.example.back.repository;
 
+import com.example.back.entity.FavoriteId;
 import com.example.back.entity.FavoriteSongEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.UUID;
+
 @Repository
-public interface FavoriteSongRepository extends JpaRepository<FavoriteSongEntity,Long> {
+public interface FavoriteSongRepository extends JpaRepository<FavoriteSongEntity, FavoriteId> {
+    List<FavoriteSongEntity> findAllByUserEmailAndSongPidIsIn(String email, List<UUID> ids);
 }
